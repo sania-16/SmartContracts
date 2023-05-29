33ebd4038cc3c10c5ace9dd6ca8799f0c19a188d
sania-16 <85894828+sania-16@users.noreply.github.com>
2023-05-29 23:15:51 +0530

    ERC20

---
+++ ERC20_SC.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract LW3Token is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(0x042826198401335737E7350D03975cDF048A22E7, 20 * 10 ** 18);
+    }
+}
